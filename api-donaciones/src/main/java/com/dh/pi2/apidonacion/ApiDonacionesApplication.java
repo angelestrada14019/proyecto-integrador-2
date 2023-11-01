4c1f852567c679e8906d4d0631d591f0a2c61a61
Scarhand21 <agusdameliocoria@gmail.com>
2023-11-01 16:57:41 -0300

    - Sprint 2: Agrego la api-donaciones

---
+++ api-donaciones/src/main/java/com/dh/pi2/apidonacion/ApiDonacionesApplication.java
@@ -0,0 +1,13 @@
+package com.dh.pi2.apidonacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDonacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDonacionesApplication.class, args);
+	}
+
+}
