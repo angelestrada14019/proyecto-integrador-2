a5b0de55d249f0198e6a143aa0290855f9c51ea6
thiagoviera <thiago_k4@live.com.ar>
2023-11-02 20:46:06 -0300

    -Sprint 2: Se agrego las entidades de api-donaciones y actualizamos el yml

---
+++ api-donaciones/src/main/java/com/dh/pi2/apidonacion/model/Donacion.java
@@ -0,0 +1,48 @@
+package com.dh.pi2.apidonacion.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "DONACIONES")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Donacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "Comentario")
+    private String comentario;
+
+    @Column(name = "Cantidad")
+    @NotNull
+    @NotBlank
+    private double cantidad;
+
+    @Column(name = "Fecha_Donacion")
+    @NotNull
+    @NotBlank
+    private LocalDate fechaDonacion;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id")
+    private MetodoPago metodoPagoID;
+
+    @Column(name = "USUARIOS_idUSUARIOS")
+    private int idUsuarios;
+
+    @Column(name = "PRODUCTOS_idPRODUCTOS")
+    private int idProductos;
+
+}
