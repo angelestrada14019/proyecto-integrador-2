c7f0bf9e19a683c2c96b0b775e3d20e691b4fa23
angelestrada14019 <rangel1998.rt@gmail.com>
2023-11-02 15:22:33 -0500

    - Sprint 2 . se quitan valores innecesarios del archivo providers

--- providers.tf
+++ providers.tf
@@ -1,14 +1,3 @@
-terraform {
-
-  required_version = ">= 0.12"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.20.0"
-    }
-  }
-}
 
 provider "aws" {
   region = var.region
