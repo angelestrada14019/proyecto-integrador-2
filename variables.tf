b41dd7c7fb30d85ea99a722bb82d52689c94e354
angelestrada14019 <rangel1998.rt@gmail.com>
2023-11-03 18:36:41 -0500

    - Sprint 2 . se cambia el cidr para la vpn y las subnets

--- variables.tf
+++ variables.tf
@@ -76,19 +76,19 @@ variable "nat_gateway_name" {
 variable "vpc_cidr_block" {
   description = "vpccidr-pi2-gp2-wowfunding"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/24"
 }
 
 variable "public_subnet_cidr" {
   description = "pbcidr-pi2-gp2-wowfunding"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.0.1.0/28"
 }
 
 variable "private_subnet_cidr" {
   description = "pcidr-pi2-gp2-wowfunding"
   type        = string
-  default     = "10.0.2.0/24"
+  default     = "10.0.2.0/28"
 }
 variable "nat_eip_name" {
   description = "eip-pi2-gp2-wowfunding"
