d27203c3b0b97944ed6feea8798b8b70289d9fdc
thiagoviera <thiago_k4@live.com.ar>
2023-11-08 19:02:24 -0300

    Cambios changes

--- api-donaciones/src/main/java/com/dh/pi2/apidonacion/ApiDonacionesApplication.java
+++ api-donaciones/src/main/java/com/dh/pi2/apidonacion/ApiDonacionesApplication.java
@@ -2,7 +2,9 @@ package com.dh.pi2.apidonacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiDonacionesApplication {
 
