4568ba5381e0bcd0a86896e48acc40a50b3aacd8
angelestrada14019 <rangel1998.rt@gmail.com>
2023-11-07 20:18:20 -0500

    - Sprint 2 . se configura el microservicio para funcionar con discovery y con config data

--- mc-productos/src/main/java/com/dh/pi2/mcproductos/McProductosApplication.java
+++ mc-productos/src/main/java/com/dh/pi2/mcproductos/McProductosApplication.java
@@ -2,10 +2,10 @@ package com.dh.pi2.mcproductos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class McProductosApplication {
 
 	public static void main(String[] args) {
