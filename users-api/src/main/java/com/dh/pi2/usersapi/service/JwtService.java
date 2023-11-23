f313f34d438f03a3740652265859dbed5d4e116f
Scarhand21 <agusdameliocoria@gmail.com>
2023-11-23 14:56:43 -0300

    cambio user para agregar seguridad 3

--- users-api/src/main/java/com/dh/pi2/usersapi/service/JwtService.java
+++ users-api/src/main/java/com/dh/pi2/usersapi/service/JwtService.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    public static final String SECRET = "4vUimFb7RhnuWEDYZ6DyqrAjPnCjWZlg";
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
 
     public void validateToken(final String token) {
