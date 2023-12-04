c45199d2f0c4d9658bdf366e6947c74b508cace4
angelestrada14019 <rangel1998.rt@gmail.com>
2023-12-04 18:56:42 -0500

    -despliegue

--- users-api/src/main/java/com/dh/pi2/usersapi/service/JwtService.java
+++ users-api/src/main/java/com/dh/pi2/usersapi/service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -14,8 +15,8 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
+    @Value("${secret.jwt}")
+    private String SECRET;
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
